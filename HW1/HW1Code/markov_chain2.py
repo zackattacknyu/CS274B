b6a26a0682363a4cf1b109a59e51556aabeaeb35
zackattacknyu <zackattacknyu@gmail.com>
2016-04-15 03:17:40 -0700

    ads

--- HW1/HW1Code/markov_chain2.py
+++ HW1/HW1Code/markov_chain2.py
@@ -235,16 +235,28 @@ print 'logp for sequence 4:'
 print logp
 
 #toy example
-toyT = np.matrix([[0.2,0.3,0.5],[0.4,0.2,0.4],[0.3,0.6,0.1]])
-toyOmat = np.matrix([[0.8,0.1,0.1],[0.1,0.4,0.5],[0.7,0.2,0.1]])
-toyP0 = np.matrix([0.1,0.2,0.7])
-toyObs = np.array([1, 2, 0, 1, 1, 0, 2])
+# toyT = np.matrix([[0.2,0.3,0.5],[0.4,0.2,0.4],[0.3,0.6,0.1]])
+# toyOmat = np.matrix([[0.8,0.1,0.1],[0.1,0.4,0.5],[0.7,0.2,0.1]])
+# toyP0 = np.matrix([0.1,0.2,0.7])
+# toyObs = np.array([1, 2, 0, 1, 1, 0, 2])
+#
+# [toyLog,toyPmatrix] = markovMarginals(x,toyObs,toyP0,toyT,toyOmat)
+#
+# print
+# print 'Toy Log P:'
+# print toyLog
+# print
+# print 'Toy P Matrix:'
+# print toyPmatrix
+
+#toyT = np.matrix([[0.9,0.05,0.05],[0.05,0.9,0.05],[0.05,0.05,0.9]])
+toyT = np.matrix([[0.05,0.9,0.05],[0.05,0.05,0.9],[0.9,0.05,0.05]])
+toyOmat = np.matrix([[0.9,0.05,0.05],[0.05,0.9,0.05],[0.05,0.05,0.9]])
+toyP0 = np.matrix([0.33,0.33,0.34])
+toyObs = np.array([1, 2, 0, 1, 2, 0, 1])
 
 [toyLog,toyPmatrix] = markovMarginals(x,toyObs,toyP0,toyT,toyOmat)
 
-print
-print 'Toy Log P:'
-print toyLog
-print
+print 
 print 'Toy P Matrix:'
 print toyPmatrix
