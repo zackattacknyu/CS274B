6600dff6c4ed8f0a56f9ff2244003e09d5f0c47f
zackattacknyu <zackattacknyu@gmail.com>
2016-04-23 19:36:54 -0700

    adfs

--- HW2/HW2Code/problem1.py
+++ HW2/HW2Code/problem1.py
@@ -15,46 +15,32 @@ print loc.shape
 
 #this find phat for each variable
 posXcount = np.sum(D,axis=0)
-probXhat1 = np.divide(posXcount,m)
-probXhat0 = 1-probXhat1
+probXj = np.zeros((n,2))
+probXj[:, 1] = np.divide(posXcount,m)
+probXj[:, 0] = 1-probXj[:,1]
 
 #this find phat(x_j,x_k) for each pair of variables
-probXjk00 = np.zeros((n,n))
-probXjk01 = np.zeros((n,n))
-probXjk10 = np.zeros((n,n))
-probXjk11 = np.zeros((n,n))
+# next dimension is probability of j, then prob of k
+probXjk = np.zeros((n,n,2,2))
 for i in range(n):
     for j in range(n):
         for k in range(m):
-            if D[k, i] == 0:
-                if D[k, j] == 0:
-                    probXjk00[i, j] += 1
-                if D[k, j] == 1:
-                    probXjk01[i, j] += 1
-            if D[k, i] == 1:
-                if D[k, j] == 0:
-                    probXjk10[i, j] += 1
-                if D[k, j] == 1:
-                    probXjk11[i, j] += 1
-probXjk00 = np.divide(probXjk00,m)
-probXjk01 = np.divide(probXjk01,m)
-probXjk10 = np.divide(probXjk10,m)
-probXjk11 = np.divide(probXjk11,m)
+            probXjk[i, j, D[k, i], D[k, j]] += 1
+probXjk = np.divide(probXjk,m)
+
+#print probXjk
 
 mutualInfo = np.zeros((n,n))
 for i in range(n):
     for j in range(n):
-        denomTerm00 = probXhat0[i]*probXhat0[j]
-        denomTerm01 = probXhat0[i] * probXhat1[j]
-        denomTerm10 = probXhat1[i] * probXhat0[j]
-        denomTerm11 = probXhat1[i] * probXhat1[j]
+        curTerm = 0
+        for k1 in range(2):
+            for k2 in range(2):
+                denomTerm = probXj[i,k1]*probXj[i,k2]
+                jointTerm = probXjk[i, j, k1, k2]
+                if denomTerm>0 and jointTerm>0:
+                    curTerm += jointTerm*np.log(jointTerm/denomTerm)
+        mutualInfo[i, j] = curTerm
 
-        term00 = probXjk00[i,j]*np.log(probXjk00[i,j]/denomTerm00)
-        term01 = probXjk01[i, j] * np.log(probXjk01[i, j] / denomTerm01)
-        term10 = probXjk10[i, j] * np.log(probXjk10[i, j] / denomTerm10)
-        term11 = probXjk11[i, j] * np.log(probXjk11[i, j] / denomTerm11)
-
-        mutualInfo[i,j]=term00+term01+term10+term11
-
-print mutualInfo
+#print mutualInfo
 
