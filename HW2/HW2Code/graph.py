6e8064b4c469683e3a47d45f71b1971a8144955c
zackattacknyu <zackattacknyu@gmail.com>
2016-04-24 02:25:59 -0700

    adfs

--- HW2/HW2Code/graph.py
+++ HW2/HW2Code/graph.py
@@ -38,15 +38,45 @@ testadj[4,12] = 1
 testadj[6,7] = 1
 testadj[3,8] = 1
 testadj[5,10] = 1
+#
+# plt.hold(True)
+# plt.plot(loc[:,1],loc[:,0],'ro')
+# for i in range(mm):
+#     for j in range(i+1,mm):
+#         node0 = loc[i,:]
+#         node1 = loc[j,:]
+#         xx = [node0[1],node1[1]];
+#         yy = [node0[0],node1[0]]
+#         if testadj[i,j]>0:
+#             plt.plot(xx,yy,'b-')
+# plt.show()
 
-plt.hold(True)
-plt.plot(loc[:,1],loc[:,0],'ro')
+# arr1 = np.array([1,2,3])
+# arr2 = np.array([4,5,6,7])
+# test1 = [arr1,arr2]
+# arr3 = np.array([45,56])
+# test1.append(arr3)
+# print test1[0]
+# print test1[2]
+# print test1[1]
+
+testadj = testadj + np.transpose(testadj)
+
+#def getAdjList()
+
+visited = np.zeros((mm))
+adjList = []
+listVertices = []
 for i in range(mm):
-    for j in range(i+1,mm):
-        node0 = loc[i,:]
-        node1 = loc[j,:]
-        xx = [node0[1],node1[1]];
-        yy = [node0[0],node1[0]]
-        if testadj[i,j]>0:
-            plt.plot(xx,yy,'b-')
-plt.show()
+    visited[i] = 1
+    adjVertices = np.where(testadj[i,:]>0)
+    verticesAdd = []
+    for j in adjVertices[0]:
+        if visited[j] <= 0:
+            verticesAdd.append(j)
+    if len(verticesAdd) > 0:
+        adjList.append(verticesAdd)
+        listVertices.append(i)
+
+for i in range(len(listVertices)):
+    print listVertices[i],adjList[i]
