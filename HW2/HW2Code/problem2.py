13aa375dff9c5d26970ed16d964e1302061c510f
zackattacknyu <zackattacknyu@gmail.com>
2016-04-25 04:19:54 -0700

    part 2b done

--- HW2/HW2Code/problem2.py
+++ HW2/HW2Code/problem2.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 
 edges = np.genfromtxt('data/edges.txt')
 loc = np.genfromtxt('data/locations.txt',delimiter=None)
+D = np.genfromtxt('data/data.txt',delimiter=None)
+m,n = D.shape # m = 2760 data points, n=30 dimensional
 
 nEdges,cc = edges.shape
 nNodes,cd = loc.shape
@@ -31,3 +33,30 @@ plt.title('Weather Station Locations with Loopy Model')
 plt.xlabel('Longitude')
 plt.ylabel('Latitude')
 plt.show()
+
+#Part B
+
+#this find phat for each variable
+posXcount = np.sum(D,axis=0)
+probXj = np.zeros((n,2))
+probXj[:, 1] = np.divide(posXcount,m)
+probXj[:, 0] = 1-probXj[:,1]
+
+#this find phat(x_j,x_k) for each pair of variables
+# next dimension is probability of j, then prob of k
+probXjk = np.zeros((n,n,2,2))
+for i in range(n):
+    for j in range(n):
+        for k in range(m):
+            probXjk[i, j, D[k, i], D[k, j]] += 1
+probXjk = np.divide(probXjk,m)
+
+#find phat(x_j|x_k)
+probXjGivenK = np.zeros((n,n,2,2))
+for i in range(n):
+    for j in range(n):
+        for jval in range(2):
+            for kval in range(2):
+                probXjGivenK[i,j,jval,kval] = probXjk[i,j,jval,kval]/probXj[j,kval]
+
+print probXjGivenK
